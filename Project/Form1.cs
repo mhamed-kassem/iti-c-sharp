b7330d9c58f3edfb322112cb2ccd61cf459a8579
Shehab el-deen <shehab.abdullah12@gmail.com>
2022-02-24 17:47:11 +0200

    First commit

---
+++ Project/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoginControl loginControl = new LoginControl();
+            this.Size = new Size(loginControl.Width, loginControl.Height);
+            Controls.Add(loginControl);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
