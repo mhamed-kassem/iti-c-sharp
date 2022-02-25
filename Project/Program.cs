2bf0d19586ec5f79923ea5d53cea3507c3674fb0
محمد ابوالقاسم سيد محمد <mhamed_kassem@yahoo.com>
2022-02-25 22:15:48 +0200

    test
    test

--- Project/Program.cs
+++ Project/Program.cs
@@ -17,6 +17,7 @@ namespace Project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DashboardForm());
+            //test
         }
     }
 }
